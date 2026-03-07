7e808d9f735331761309f05877754277e3214e1a
SUSHIL THAKUR <mt3028788@gmail.com>
2026-03-07 14:10:17 +0530

    add Avalonia .NET MVVM App (Avalonia UI) , template pure fresh projects

---
+++ AvaloniaUI.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaUI.App.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ AvaloniaUI.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaUI.App.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
