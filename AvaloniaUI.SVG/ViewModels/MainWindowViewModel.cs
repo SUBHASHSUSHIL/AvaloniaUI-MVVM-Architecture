4b5585be194d3a5b4371372d5dfc6cacdc82a72f
SUSHIL THAKUR <mt3028788@gmail.com>
2026-03-15 08:57:17 +0530

    project name change and sample add with svg pic use

---
+++ AvaloniaUI.SVG/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaloniaUI.SVG.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ AvaloniaUI.SVG/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaUI.SVG.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
